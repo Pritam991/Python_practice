0d758adb0662fafc5bbac90fd8d6dde4ca21743e
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-10-19 03:06:23 +0530

    code

---
+++ DrawBox.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+from numpy import square
+
+speed('slowest')
+
+square(100)
+
+lt(30)
+fd(100)
+rt(30)
+
+square(100)
+
+lt(90)
+fd(100)
+lt(90+30)
+fd(100)
+rt(30+180)
+fd(100)
+lt(30)
+fd(100)
+rt(90+30)
+fd(100)
+rt(60)
+fd(100)
